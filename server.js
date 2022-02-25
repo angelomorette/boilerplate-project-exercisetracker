fd6eab79ba79fc9147d19b6c99e7c5251056fa4d
angelomorette <angelomorette@gmail.com>
2022-02-25 20:29:44 -0300

    add novas dependencias e configuraçoes iniciais

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(cors())
 app.use(express.static('public'))
