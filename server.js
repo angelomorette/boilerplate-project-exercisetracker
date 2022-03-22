c514d368118c38e10c6d40b65b004fc6598dc9be
angelomorette <angelomorette@gmail.com>
2022-03-22 12:06:56 -0300

    Adicionar usuario exercicio e consulta por usuario

--- server.js
+++ server.js
@@ -23,10 +23,114 @@ const UserSchema = new mongoose.Schema({
 });
 
 const ExerciseSchema = new mongoose.Schema({
-  userId: { type: String, required: true},
+  userId: { type: String, required: true },
   description: String,
   duration: Number,
   date: Date
+});
+
+//model
+const User = mongoose.model('User', UserSchema);
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
+
+//Adicionando usuario 
+app.post('/api/users', (req, res) => {
+
+  const user = new User({
+    username: req.body.username
+  });
+
+  user.save((error, data) => {
+    if (error || !data) {
+      res.send("Erro ao tentar salvar um novo usuário");
+    } else {
+      res.json({
+        username: data.username,
+        _id: data._id
+      });
+    }
+  });
+
+});
+
+//Adicionando exercicio
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+
+  User.findById(id, (error, data) => {
+    if (error || !data) {
+      res.send("Error ao tentar encontrar o usuario");
+    } else {
+
+      const exercise = new Exercise({
+        userId: id,
+        description,
+        duration,
+        date: new Date(date)
+      })
+
+
+      exercise.save((error, data) => {
+        if (error || !data) {
+          res.send("Error ao tentar adicionar novo exercicio")
+        } else {
+          const { description, duration, date, _id } = data
+          res.json({
+            username: data.username,
+            description,
+            duration,
+            date: date.toDateString(),
+            _id: data.id
+          });
+        }
+      });
+    }
+  });
+});
+
+//consulta por usuario 
+app.get('/api/users/:_id/logs', (req, res) => {
+  const id = req.params._id;
+  const to = new Date(req.query.to);
+  const from = new Date(req.query.from);
+  const limit = Number(req.query.limit);
+
+  console.log(to);
+  console.log(from);
+
+  User.findById(id, (error, user) => {
+    if (error || !user) {
+      res.send("Error ao tentar encontrar o usuario");
+    } else {
+
+      Exercise.find({
+        userId: id,
+        date: {
+          $lt: to != "Invalid Date" ? to : Date.now(),
+          $gt: from != "Invalid Date" ? from : 0,
+        }
+      })
+        .sort("-date")
+        .limit(limit)
+        .exec(function (error, data) {
+
+          const result = {
+            _id: id,
+            username: user.username,
+            from: from != "Invalid Date" ? from.toDateString() : undefined,
+            to: to != "Invalid Date" ? to.toDateString() : undefined,
+            count: data.length,
+            log: data.map((e) => ({
+              description: e.description,
+              duration: e.duration,
+              date: e.date.toDateString(),
+            })),
+          }
+          res.json(result);
+        })
+    }
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
